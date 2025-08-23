23db4518c4889831ccb91dd1884aa287054e010d
Josh Hernandez <yatchualee@gmail.com>
2025-08-23 02:19:21 -0400

    Add new file

---
+++ tests/minimal_browserstack_firefox_test.py
@@ -0,0 +1,40 @@
+import os
+
+from selenium import webdriver
+
+
+USERNAME = os.environ.get('BSTACK_USER')
+ACCESS_KEY = os.environ.get('BSTACK_KEY')
+BROWSER = os.environ.get('TEST_BUILD', 'chrome').lower()
+
+capabilities_map = {
+    'chrome': {
+        'browserName': 'Chrome',
+        'os': 'Windows',
+        'osVersion': '11',
+    },
+    'firefox': {
+        'browserName': 'Firefox',
+        'os': 'Windows',
+        'osVersion': '11',
+    },
+    'edge': {
+        'browserName': 'Edge',
+        'os': 'Windows',
+        'osVersion': '11',
+    },
+}
+
+capabilities = capabilities_map.get(BROWSER)
+if not capabilities:
+    raise ValueError(f'Unsupported browser: {BROWSER}')
+
+remote_url = f'https://{USERNAME}:{ACCESS_KEY}@hub-cloud.browserstack.com/wd/hub'
+
+driver = webdriver.Remote(
+    command_executor=remote_url, desired_capabilities=capabilities
+)
+
+driver.get('https://www.example.com')
+print(driver.title)
+driver.quit()
